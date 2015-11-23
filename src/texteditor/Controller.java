ee1de3425fc7bdb16e433057db1f5fd806feb248
Diego Zavaleta <24547744m@github.org>
2015-11-23 21:17:50 +0100

    editor de textos con opciones version beta para linux

--- src/texteditor/Controller.java
+++ src/texteditor/Controller.java
@@ -2,9 +2,22 @@ package texteditor;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import sun.font.FontFamily;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,13 +25,24 @@ import java.io.IOException;
 public class Controller {
 
     public TextArea textAreaContent;
+    public MenuItem copyEditMenu;
+    public MenuItem cutEditMenu;
+    public MenuItem copyTextArea;
+    public MenuItem cutTextArea;
+    public CheckMenuItem sansFontFamily;
+    public CheckMenuItem freeFontFamily;
+    public CheckMenuItem twelveFontSize;
+    public CheckMenuItem fourteenFontSize;
     private Stage stage;//Only for FileChooser
     private FileChooser fileChooser = new FileChooser();
     private File openFile;
     private File saveFile;
 
+
+
     public void initialize(){
         System.out.println("iniciando . .");
+
     }
 
 
@@ -45,6 +69,7 @@ public class Controller {
 //                stage.setTitle("holaaaaaaaaaaaaaaaaaa");
                 FileClass fileClass = new FileClass();
                 String openContentFile = fileClass.getContentFile(openFile);
+                System.out.println("texto" + openContentFile);
                 textAreaContent.setText(openContentFile);
             }else
                 System.out.println("No Files Selected");
@@ -91,12 +116,71 @@ public class Controller {
 
 
     /*********************** EDIT MENU ***********************/
+    public void enableDisableItems(Event event) {
+        if (textAreaContent.getSelectedText().equals("")){
+            copyEditMenu.setDisable(true);
+            cutEditMenu.setDisable(true);
+            copyTextArea.setDisable(true);
+            cutTextArea.setDisable(true);
+        }else{
+            copyEditMenu.setDisable(false);
+            cutEditMenu.setDisable(false);
+            copyTextArea.setDisable(false);
+            cutTextArea.setDisable(false);
+        }
+    }
+    public void copyTextAction(ActionEvent ae) {
+        textAreaContent.copy();
+    }
+    public void cutTextAction(ActionEvent ae) {
+        textAreaContent.cut();
+    }
+    public void pasteTextAction(ActionEvent ae) {
+        textAreaContent.paste();
+    }
+    public void undoTextAction(ActionEvent ae) {
+        textAreaContent.undo();
+    }
+    public void redoTextAction(ActionEvent ae) {
+        textAreaContent.redo();
+    }
+
+    /*********************** OPTIONS MENU ***********************/
+    public void fontFamilyAction(ActionEvent ae) {
+        CheckMenuItem item = (CheckMenuItem) ae.getSource();
+        sansFontFamily.setSelected(false);
+        freeFontFamily.setSelected(false);
+        item.setSelected(true);
+        textAreaContent.setFont(new Font(item.getText(), textAreaContent.getFont().getSize()));
+
+    }
+
+    public void fontSizeAction(ActionEvent ae) {
+        CheckMenuItem item = (CheckMenuItem) ae.getSource();
+        fourteenFontSize.setSelected(false);
+        twelveFontSize.setSelected(false);
+        item.setSelected(true);
+        double fontSize = Double.parseDouble( item.getText() );
+        textAreaContent.setFont(new Font(fontSize));
+    }
+
+
+    /*********************** HELP MENU ***********************/
+    public void aboutHelpAction(ActionEvent ae) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About TextEditor");
+        alert.setHeaderText("TextEditor v. 0.8");
+        alert.setContentText("Copyright © 2015-2016 TextEditor Beta\n" +
+                             "Developed by: Diego Zavaleta");
+        alert.showAndWait();
+    }
 
 
 
 
     private void configureFileChooser(FileChooser fileChooser, String msj) {
         fileChooser.setTitle(msj);
+
         fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("All Files (*.*)", "*.*"),
@@ -113,5 +197,6 @@ public class Controller {
     public void setStage(Stage stage) {
         this.stage = stage;
     }
+
 
 }
--- src/texteditor/Main.java
+++ src/texteditor/Main.java
@@ -1,23 +1,33 @@
 package texteditor;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     private Stage primaryStage;
+    private Scene scene;
+    private Controller controller;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         this.primaryStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("xml_editor.fxml"));
         primaryStage.setTitle("Simple Text Editor");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        Controller controller = new Controller();
+        scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        controller = new Controller();
         controller.setStage(primaryStage);//Enviando el Stage al Controller
+
         primaryStage.show();
     }
 
